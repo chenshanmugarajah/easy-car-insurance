74f22689f7b272c798a54a9079ffcdb844ddc360
chen shanmugarajah <chenshanmugarajah@gmail.com>
2022-03-20 01:10:47 +0000

    refactored and set up selenium

--- src/main/java/chen/shanmugarajah/App.java
+++ src/main/java/chen/shanmugarajah/App.java
@@ -1,24 +1,13 @@
 package chen.shanmugarajah;
 
-import java.io.IOException;
-
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args ) throws IOException
+    public static void main( String[] args )
     {
-    	
-    	ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-    	
-    	Scraper argos = (Scraper) context.getBean("Argos");
-    	
-    	argos.getData();
     	
     }
 }
