dc0f52aff61c072a36e23214f242ec52707d854d
chen shanmugarajah <chenshanmugarajah@gmail.com>
2022-03-20 01:26:09 +0000

    Set up selenium with local driver

--- src/main/java/chen/shanmugarajah/App.java
+++ src/main/java/chen/shanmugarajah/App.java
@@ -1,13 +1,20 @@
 package chen.shanmugarajah;
 
-/**
- * Hello world!
- *
- */
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class App 
 {
     public static void main( String[] args )
     {
+    	
+    	System.setProperty("webdriver.chrome.driver", "C:\\\\Users\\\\Chen\\\\Downloads\\\\chromedriver.exe");
+    	WebDriver driver = new ChromeDriver();
+    	driver.get("https://mygocompare.gocompare.com/login/");
+    	
+    	System.out.println("Got page");
+    	
+    	driver.quit();
     	
     }
 }
